b54be9284db8fba867c047370d59dba54a53174e
gusthsb <gustavo.hdsbrito@gmail.com>
2026-09-13 23:22:15 -0300

    feat: esboçando class

---
+++ maze_generator.py
@@ -0,0 +1,24 @@
+import random
+import typing
+
+
+class MazeGenerator:
+    """
+    Class to manage the maze generation.
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def generate(self) -> None:
+        """
+        Function to execute de maze generation algorithm.
+        """
+        pass
+
+    def get_structure(self) -> None:
+        """
+        Access the maze structure
+        """
+        pass
+    
