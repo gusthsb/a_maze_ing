e5c29fd8ba9bb81c89172033313a5eff357d7980
gusthsb <gustavo.hdsbrito@gmail.com>
2026-09-15 22:21:43 -0300

    feat: atributos no Maze

--- maze_generator.py
+++ maze_generator.py
@@ -7,8 +7,25 @@ class MazeGenerator:
     Class to manage the maze generation.
     """
 
-    def __init__(self) -> None:
-        pass
+    def __init__(
+            self,
+            width: int,
+            height: int,
+            entry: tuple[int, int],
+            exit_pos: tuple[int, int],
+            perfect: bool,
+            seed: typing.Optional[int] = None
+    ) -> None:
+        self.grid: list[list[int]] = [
+            [15 for _ in range(width) for _ in range(height)]
+        ]
+        self.directions = [
+            (0, -1, 1, 4),
+            (1, 0, 2, 8),
+            (0, 1, 4, 1),
+            (-1, 0, 8, 2)
+        ]
+        
 
     def generate(self) -> None:
         """
